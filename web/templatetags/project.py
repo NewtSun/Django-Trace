01d6eda8ea94003a6960b23fa06e3b610ee2fc7f
NewtSun <1807118870@qq.com>
2020-04-28 19:15:35 +0800

    项目菜单及展示

--- web/templatetags/project.py
+++ web/templatetags/project.py
@@ -21,12 +21,12 @@ def all_project_list(request):
 @register.inclusion_tag('inclusion/manage_menu_list.html')
 def manage_menu_list(request):
     data_list = [
-        {'title': '概览', 'url': reverse("dashboard", kwargs={'project_id': request.tracer.project.id})},
-        {'title': '问题', 'url': reverse("issues", kwargs={'project_id': request.tracer.project.id})},
-        {'title': '统计', 'url': reverse("statistics", kwargs={'project_id': request.tracer.project.id})},
-        {'title': 'wiki', 'url': reverse("wiki", kwargs={'project_id': request.tracer.project.id})},
-        {'title': '文件', 'url': reverse("file", kwargs={'project_id': request.tracer.project.id})},
-        {'title': '配置', 'url': reverse("setting", kwargs={'project_id': request.tracer.project.id})},
+        {'title': '概览', 'url': reverse("project.dashboard", kwargs={'project_id': request.tracer.project.id})},
+        {'title': '问题', 'url': reverse("project.issues", kwargs={'project_id': request.tracer.project.id})},
+        {'title': '统计', 'url': reverse("project.statistics", kwargs={'project_id': request.tracer.project.id})},
+        {'title': 'wiki', 'url': reverse("project.wiki", kwargs={'project_id': request.tracer.project.id})},
+        {'title': '文件', 'url': reverse("project.file", kwargs={'project_id': request.tracer.project.id})},
+        {'title': '配置', 'url': reverse("project.setting", kwargs={'project_id': request.tracer.project.id})},
     ]
 
     for item in data_list:
--- web/urls.py
+++ web/urls.py
@@ -1,7 +1,8 @@
-from django.urls import path
+from django.urls import path, include
 from web.views import account
 from web.views import home
 from web.views import project
+from web.views import manage
 
 urlpatterns = [
     # 加name方便反向解析
@@ -14,6 +15,15 @@ urlpatterns = [
     path('logout/', account.logout, name='logout'),
 
     path('project/list', project.project_list, name='project_list'),
-    path('project/star/(?P<project_type>)/(?P<project_id>)/', project.project_star, name='project_star'),
-    path('project/unstar/(?P<project_type>)/(?P<project_id>)/', project.project_unstar, name='project_unstar'),
+    path('project/star/<project_type>/<project_id>/', project.project_star, name='project_star'),
+    path('project/unstar/<project_type>/<project_id>/', project.project_unstar, name='project_unstar'),
+
+    path('manage/<project_id>/', include([
+        path('dashboard/', manage.dashboard, name='project.dashboard'),
+        path('issues/', manage.issues, name='project.issues'),
+        path('statistics/', manage.statistics, name='project.statistics'),
+        path('file/', manage.file, name='project.file'),
+        path('wiki/', manage.wiki, name='project.wiki'),
+        path('setting/', manage.setting, name='project.setting'),
+    ]))
 ]
